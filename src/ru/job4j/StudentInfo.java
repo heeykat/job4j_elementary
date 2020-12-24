c47710a4cbdfc02e7d1e0234323c031a98ed4ca4
heeykat <heeykat@yandex.ru>
2020-12-24 18:57:52 +0300

    3.2. Фиксация изменений на github [#437987]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Katya Savchuk");
+        System.out.println("22.07.1990");
+
+    }
+}
