5fa7e993ff350a1c5238784aaa7816c45afa93ce
heeykat <heeykat@yandex.ru>
2020-12-30 15:14:33 +0300

    1. Модульные тесты. [#437993]

--- src/ru/job4j/Echo.java
+++ src/ru/job4j/Echo.java
@@ -1,4 +1,18 @@
 package ru.job4j;
 
 public class Echo {
+
+    public static void main(String[] args) {
+
+        System.out.println(Echo.checkGame(0.2, 50, 9));
+    }
+
+    public static int checkGame(double percent, int prize, int pay) {
+        int v = (int) (percent * prize);
+        if (v > pay) {
+            return v - pay;
+        } else {
+            return 0;
+        }
+    }
 }
