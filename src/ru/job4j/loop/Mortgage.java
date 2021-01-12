562252786fea68baf41a7899b5cc72902bc47d87
heeykat <heeykat@yandex.ru>
2021-01-12 11:30:01 +0300

    5.7. Ипотека [#438019]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 1;
-        double debt = amount * percent - salary;
+        double percentFr = percent / 100;
+        double debt = (amount * percentFr + amount) - salary;
         while (debt > 0) {
-            debt -= debt;
             year++;
+            debt = (debt * percentFr + debt) - salary;
         }
         return year;
     }
