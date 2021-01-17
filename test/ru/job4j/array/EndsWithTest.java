4bcbe361b4b63bb7e29180ee475719fd875ac15e
heeykat <heeykat@yandex.ru>
2021-01-17 11:04:48 +0300

    6.4.2. Слово заканчивается на ... [#437933]

---
+++ test/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+
+    @Test
+    public void whenEndWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotEndWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenEndWithPrefix3ThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l' ,'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotEndWithPrefix3ThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'o' ,'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
