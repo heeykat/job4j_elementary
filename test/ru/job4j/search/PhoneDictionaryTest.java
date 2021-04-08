b755885f88a3ec846ae65f0af1593d0d18400786
heeykat <heeykat@yandex.ru>
2021-04-08 16:31:07 +0300

    1. Телефонный справочник на базе ArrayList [#438048]fix

--- test/ru/job4j/search/PhoneDictionaryTest.java
+++ test/ru/job4j/search/PhoneDictionaryTest.java
@@ -17,4 +17,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenNoValue() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Katya");
+        assertThat(persons.size(), is(0));
+    }
 }
